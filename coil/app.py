eab50a0467f3ea371d45972f6aab37d1ac5af698
evryscope <evryscope@stars-dw4.edc.renci.org>
2017-09-30 19:45:07 -0400

    end to end one

--- coil/app.py
+++ coil/app.py
@@ -1,14 +1,18 @@
-import os
 import cwltool.factory
-import cwltool.draft2tool
-from cwltool.draft2tool import CommandLineTool
-import cwltool.workflow
-from cwltool.process import get_feature, scandeps, UnsupportedRequirement, normalizeFilesDirs, shortname
-from cwltool.load_tool import fetch_document
-from cwltool.pathmapper import adjustFileObjs, adjustDirObjs, visit_class
-from cwltool.utils import aslist
-from cwltool.builder import substitute
-from cwltool.pack import pack
+#import cwltool.draft2tool
+#import cwltool.workflow
+
+import json
+import os
+import requests
+#from cwltool.draft2tool import CommandLineTool
+#from cwltool.process import get_feature, scandeps, UnsupportedRequirement, normalizeFilesDirs, shortname
+#from cwltool.load_tool import fetch_document
+#from cwltool.pathmapper import adjustFileObjs, adjustDirObjs, visit_class
+#from cwltool.utils import aslist
+#from cwltool.builder import substitute
+#from cwltool.pack import pack
+from stars import Stars
 
 # https://github.com/asher/chronos-python
 # http://andrewjesaitis.com/2017/02/common-workflow-language---a-tutorial-on-making-bioinformatics-repeatable/
@@ -21,20 +25,18 @@ class Coil(object):
         self.factory = cwltool.factory.Factory ()
 
     def execute (self, workflow_spec, args={}):
-        workflow = self.factory.make (workflow_spec) 
-        result = workflow (**args)
-        return result
+        return self.factory.make (workflow_spec) (**args)
 
-coil = Coil ()
-coil.execute (workflow_spec = "wf/one/wf.cwl",
-              args = { 
-                  "seed" :  3,
-                  "input1" : {
-                      "class"    : "File",
-                      "path"     : "wf/one/job.json",
-                      "contents" : "x099809808",
-                      "basename" : "random_lines_job.json"
-                  },
-                  "num_lines"   : 40,
-                  "output_file" : "out.txt"
-              })
+Coil ().execute (
+    workflow_spec = "wf/one/wf.cwl",
+    args = { 
+        "seed" :  3,
+        "input1" : {
+            "class"    : "File",
+            "path"     : "wf/one/job.json",
+            "contents" : "x099809808",
+            "basename" : "random_lines_job.json"
+        },
+        "num_lines"   : 40,
+        "output_file" : "out.txt"
+    })
---
+++ coil/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+set -e
+
+PATH=$PWD/wf/one:$PATH
+export PYTHONPATH=/projects/stars/app/cwltool
+export PYTHONPATH=/projects/stars/app/stars/cluster/src/stars:$PYTHONPATH
+python app.py
+
+exit 0
